8cd8aaf344c099bac9188b862297e87d55c46093
Joseph Jaoudi <jd@yohana.com>
2023-06-29 09:51:38 -0700

    Rename eventrouter_freertos.c to eventrouter_os.c

--- eventrouter.c
+++ eventrouter.c
@@ -1,7 +1,7 @@
 #include "eventrouter/internal/checked_config.h"
 
 #ifdef ER_CONFIG_OS
-#include "eventrouter/internal/eventrouter_freertos.c"
+#include "eventrouter/internal/eventrouter_os.c"
 #elif ER_IMPLEMENTATION == ER_IMPL_BAREMETAL
 #include "eventrouter/internal/eventrouter_baremetal.c"
 #include "eventrouter/internal/list.c"
