765380a1ed60192b8b5b02fd77aa6a12e4e022df
Joseph Jaoudi <jd@yohana.com>
2023-06-29 10:17:54 -0700

    Move container_of() definition from checked_config.h to defs.h
    container_of() isn't a configuration parameter and isn't used in the validation
    of configuration parameters.

--- eventrouter/internal/checked_config.h
+++ eventrouter/internal/checked_config.h
@@ -35,15 +35,6 @@
 #endif
 #endif
 
-/// Returns the address of the structure containing the member.
-#ifndef container_of
-#define container_of(ptr, type, member)                    \
-    ({                                                     \
-        const typeof(((type *)0)->member) *__mptr = (ptr); \
-        (type *)((char *)__mptr - offsetof(type, member)); \
-    })
-#endif
-
 //==============================================================================
 // Implementation Selection
 //==============================================================================
