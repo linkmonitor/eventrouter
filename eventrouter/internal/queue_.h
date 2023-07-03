48184f29e7109c4deee0e2ed9652df6cc848909c
Joseph Jaoudi <linkmonitor@gmail.com>
2023-07-03 15:42:51 -0700

    Add os-implementation-agnostic queue abstraction

---
+++ eventrouter/internal/queue_.h
@@ -0,0 +1,31 @@
+#ifndef EVENTROUTER_QUEUE_H
+#define EVENTROUTER_QUEUE_H
+
+#include "event.h"
+
+#include <stdbool.h>
+#include <stddef.h>
+
+/// An opaque queue type for ErEvent_t* elements.
+typedef void* ErQueue_t;
+typedef ErQueue_t ErQueueHandle_t;
+
+/// Allocates a new `ErQueue_t` which can hold at most `a_capacity` elements.
+ErQueue_t ErQueueNew(size_t a_capacity);
+
+/// Frees a `ErQueue_t` previously allocated with `ErQueueNew()`.
+void ErQueueFree(ErQueue_t a_queue);
+
+/// Blocks until there is a value to read from `a_queue`, then returns it.
+ErEvent_t* ErQueuePopFront(ErQueue_t a_queue);
+
+/// Blocks until there is space to write `a_event` to the queue, then returns.
+void ErQueuePushBack(ErQueue_t a_queue, ErEvent_t* a_event);
+
+/// Returns true if `a_event` was read from `a_queue` within `a_ms`.
+bool ErQueueTimedPopFront(ErQueue_t a_queue, ErEvent_t** a_event, int64_t a_ms);
+
+/// Returns true if `a_event` was written to `a_queue` within `a_ms`.
+bool ErQueueTimedPushBack(ErQueue_t a_queue, ErEvent_t* a_event, int64_t a_ms);
+
+#endif /* EVENTROUTER_QUEUE_H */
---
+++ eventrouter/internal/queue_freertos.c
@@ -0,0 +1,56 @@
+#include "queue_.h"
+
+#include <assert.h>
+
+#include "FreeRTOS.h"
+#include "queue.h"
+
+#include "event.h"
+
+//==============================================================================
+// Macros And Defines
+//==============================================================================
+
+#ifndef pdMS_TO_TICKS
+#define pdMS_TO_TICKS(a_time_ms)                                               \
+    ((TickType_t)(((TickType_t)(a_time_ms) * (TickType_t)configTICK_RATE_HZ) / \
+                  (TickType_t)1000U))
+#endif
+
+//==============================================================================
+// Public Functions
+//==============================================================================
+
+ErQueue_t ErQueueNew(size_t a_capacity)
+{
+    return xQueueCreate(a_capacity, sizeof(ErEvent_t *));
+}
+
+void ErQueueFree(ErQueue_t a_queue)
+{
+    vQueueDelete(a_queue);
+}
+
+ErEvent_t *ErQueuePopFront(ErQueue_t a_queue)
+{
+    ErEvent_t *event = NULL;
+    assert(pdTRUE == xQueueReceive(a_queue, &event, portMAX_DELAY));
+    return event;
+}
+
+void ErQueuePushBack(ErQueue_t a_queue, ErEvent_t *a_event)
+{
+    assert(pdTRUE == xQueueSend(a_queue, &a_event, portMAX_DELAY));
+}
+
+bool ErQueueTimedPopFront(ErQueue_t a_queue, ErEvent_t **a_event, int64_t a_ms)
+{
+    BaseType_t ret   = xQueueReceive(a_queue, &a_event, pdMS_TO_TICKS(a_ms));
+    return (ret == pdTRUE);
+}
+
+bool ErQueueTimedPushBack(ErQueue_t a_queue, ErEvent_t *a_event, int64_t a_ms)
+{
+    BaseType_t ret = xQueueSend(a_queue, &a_event, pdMS_TO_TICKS(a_ms));
+    return (ret = pdTRUE);
+}
