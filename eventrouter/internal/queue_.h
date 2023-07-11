fc628f3a0f5202f303dd0086fd019b68a11e902e
Joseph Jaoudi <jd@yohana.com>
2023-07-11 13:40:07 -0700

    Add extern "C" guards to queue_.h

--- eventrouter/internal/queue_.h
+++ eventrouter/internal/queue_.h
@@ -6,26 +6,37 @@
 #include <stdbool.h>
 #include <stddef.h>
 
-/// An opaque queue type for ErEvent_t* elements.
-typedef void* ErQueue_t;
-typedef ErQueue_t ErQueueHandle_t;
+#ifdef __cplusplus
+extern "C"
+{
+#endif
 
-/// Allocates a new `ErQueue_t` which can hold at most `a_capacity` elements.
-ErQueue_t ErQueueNew(size_t a_capacity);
+    /// An opaque queue type for ErEvent_t* elements.
+    typedef void* ErQueue_t;
+    typedef ErQueue_t ErQueueHandle_t;
 
-/// Frees a `ErQueue_t` previously allocated with `ErQueueNew()`.
-void ErQueueFree(ErQueue_t a_queue);
+    /// Allocates a new `ErQueue_t` that can hold at most `a_capacity` elements.
+    ErQueue_t ErQueueNew(size_t a_capacity);
 
-/// Blocks until there is a value to read from `a_queue`, then returns it.
-ErEvent_t* ErQueuePopFront(ErQueue_t a_queue);
+    /// Frees a `ErQueue_t` previously allocated with `ErQueueNew()`.
+    void ErQueueFree(ErQueue_t a_queue);
 
-/// Blocks until there is space to write `a_event` to the queue, then returns.
-void ErQueuePushBack(ErQueue_t a_queue, ErEvent_t* a_event);
+    /// Blocks until there is a value to read from `a_queue`, then returns it.
+    ErEvent_t* ErQueuePopFront(ErQueue_t a_queue);
 
-/// Returns true if `a_event` was read from `a_queue` within `a_ms`.
-bool ErQueueTimedPopFront(ErQueue_t a_queue, ErEvent_t** a_event, int64_t a_ms);
+    /// Blocks until there is space to write to the queue, then returns.
+    void ErQueuePushBack(ErQueue_t a_queue, ErEvent_t* a_event);
 
-/// Returns true if `a_event` was written to `a_queue` within `a_ms`.
-bool ErQueueTimedPushBack(ErQueue_t a_queue, ErEvent_t* a_event, int64_t a_ms);
+    /// Returns true if `a_event` was read from `a_queue` within `a_ms`.
+    bool ErQueueTimedPopFront(ErQueue_t a_queue, ErEvent_t** a_event,
+                              int64_t a_ms);
+
+    /// Returns true if `a_event` was written to `a_queue` within `a_ms`.
+    bool ErQueueTimedPushBack(ErQueue_t a_queue, ErEvent_t* a_event,
+                              int64_t a_ms);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* EVENTROUTER_QUEUE_H */
