77e273eac93a29f14ac80f85e8dd832a7899cf6a
Andrew Warren <aiw@yohana.com>
2024-02-22 12:09:42 -0800

    Type-checking container_of macro
    It is an error to invoke the container_of() macro with a
    `ptr` parameter that is not of type pointer-to-`member`.
    But the macro hides that error by ignoring the 'ptr' type.
    
    Make the macro safer by requiring `ptr` to have the
    correct type.
    
    Also, rename it to er_container_of(), to avoid potential
    name collisions with other container_of() macros.

--- eventrouter/internal/defs.h
+++ eventrouter/internal/defs.h
@@ -4,12 +4,16 @@
 #define ER_UNUSED(statement) (void)(statement);
 
 /// Returns the address of the structure containing the member.
-#ifndef container_of
-#define container_of(ptr, type, member)                    \
+
+/// The "?:" conditional operator requires its second and third operands to
+/// be of compatible type, so by writing "1 ? (ptr) : &((type *)0)->member"
+/// instead of simply writing "(ptr)", we ensure that `ptr` actually does
+/// point to a type compatible with `member` (compilation halts if it doesn't).
+
+#define er_container_of(ptr, type, member)                 \
     ({                                                     \
-        const typeof(((type *)0)->member) *__mptr = (ptr); \
+        void *__mptr = (1 ? (ptr) : &((type *)0)->member); \
         (type *)((char *)__mptr - offsetof(type, member)); \
     })
-#endif
 
 #endif /* EVENTROUTER_DEFS_H */
